811dcbffd2020b68133acbc9ab3a9ae0a806aeac
HugoJamielniak <hugojamielniak@hotmail.com>
2026-05-19 14:11:48 -0300

    commit Hugo

---
+++ MedXF/backend/src/main/java/com/pucpr/medxf/controller/Medico.java
@@ -0,0 +1,31 @@
+package com.pucpr.medxf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/medico")
+public class Medico {
+
+    @GetMapping("/home")
+    public String paginaHomeMedico() {
+        return "/html/home-medico/home-medico";
+    }
+
+    @GetMapping("/login")
+    public String paginaLoginMedico() {
+        return "/html/login-medico/login-medico";
+    }
+
+    @GetMapping("/triagem")
+    public String paginaNovaTriagemMedico() {
+        return "html/nova-triagem/nova-triagem";
+    }
+
+    @GetMapping("/avaliacao")
+    public String paginaAvaliacaoMedico() {
+        return "html/avaliação-medico/avaliação-medico";
+    }
+
+}
